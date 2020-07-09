3c71d806b8f7e626c35593b59eb8f4703637f8e2
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-07-10 01:46:46 +0200

    Proper type hints added to the boson sampling utilities.

--- src/Boson_Sampling_Utilities.py
+++ src/Boson_Sampling_Utilities.py
@@ -1,12 +1,20 @@
 __author__ = 'Tomasz Rybotycki'
 
-# TR TODO: Add proper type hints.
-# TR TODO: Consider making this file a package.
-import numpy as np
+# TR TODO: Consider making this file a package along with exact distribution calculator.
+
 import itertools
+from typing import List
+from numpy import ndarray, zeros
 
 
-def permanent_recursive_part(mtx, column, selected, prod):
+def calculate_permanent(matrix: ndarray) -> float:
+    """
+    Returns the permanent of the matrix mat.
+    """
+    return permanent_recursive_part(matrix, 0, [], 1)
+
+
+def permanent_recursive_part(mtx: ndarray, column: int, selected: List[int], prod: int) -> float:
     """
     Row expansion for the permanent of matrix mtx.
     The counter column is the current column,
@@ -15,24 +23,17 @@ def permanent_recursive_part(mtx, column, selected, prod):
     """
     if column == mtx.shape[1]:
         return prod
-    else:
-        result = 0
-        for row in range(mtx.shape[0]):
-            if row not in selected:
-                result = result \
-                         + permanent_recursive_part(mtx, column + 1, selected + [row], prod * mtx[row, column])
-        return result
+
+    result = 0
+    for row in range(mtx.shape[0]):
+        if row not in selected:
+            result = result \
+                     + permanent_recursive_part(mtx, column + 1, selected + [row], prod * mtx[row, column])
+    return result
 
 
-def calculate_permanent(mat):
-    """
-    Returns the permanent of the matrix mat.
-    """
-    return permanent_recursive_part(mat, 0, [], 1)
-
-
-def particle_state_to_modes_state(particle_state: np.ndarray, observed_modes_number: int) -> np.ndarray:
-    modes_state = np.zeros(observed_modes_number)
+def particle_state_to_modes_state(particle_state: ndarray, observed_modes_number: int) -> ndarray:
+    modes_state = zeros(observed_modes_number)
 
     # Adding the particle to it's mode.
     for particle in particle_state:
@@ -42,7 +43,7 @@ def particle_state_to_modes_state(particle_state: np.ndarray, observed_modes_num
     return modes_state
 
 
-def modes_state_to_particle_state(mode_state: np.ndarray, particles_number: int) -> np.ndarray:
+def modes_state_to_particle_state(mode_state: ndarray, particles_number: int) -> ndarray:
     """
         Return given mode-basis state in particle basis.
 
@@ -52,9 +53,8 @@ def modes_state_to_particle_state(mode_state: np.ndarray, particles_number: int)
     """
 
     number_of_observed_modes = len(mode_state)
-    modes = np.zeros(number_of_observed_modes)
-    modes[:] = mode_state[:]
-    particles_state = np.zeros(particles_number)
+    modes = mode_state.copy()
+    particles_state = zeros(particles_number)
 
     i = k = 0
     while i < number_of_observed_modes:
@@ -69,10 +69,10 @@ def modes_state_to_particle_state(mode_state: np.ndarray, particles_number: int)
     return particles_state
 
 
-def generate_possible_outputs(number_of_particles: int, number_of_modes: int) -> list:
+def generate_possible_outputs(number_of_particles: int, number_of_modes: int) -> List[ndarray]:
     outputs = []
 
-    output = np.zeros(number_of_modes)
+    output = zeros(number_of_modes)
     output[0] = number_of_particles
     outputs.append(output)
 
@@ -92,8 +92,7 @@ def generate_possible_outputs(number_of_particles: int, number_of_modes: int) ->
     return outputs
 
 
-# All the possible states from initial_state with NUMBER_OF_PARTICLES_LEFT photons
-def generate_lossy_inputs(initial_state: list, number_of_particles_left: int) -> list:
+def generate_lossy_inputs(initial_state: ndarray, number_of_particles_left: int) -> List[List[int]]:
     """
     From initial state generate all possible input states after losses application.
     :param initial_state: The state we start with.
@@ -104,8 +103,7 @@ def generate_lossy_inputs(initial_state: list, number_of_particles_left: int) ->
     number_of_modes = len(initial_state)
     initial_number_of_particles = len(initial_state) - initial_state.count(0)
     for i in range(number_of_modes):
-        for j in range(int(initial_state[i])):
-            x0.append(i)
+        x0.extend([i] * int(initial_state[i]))
 
     lossy_inputs_list = []
 
