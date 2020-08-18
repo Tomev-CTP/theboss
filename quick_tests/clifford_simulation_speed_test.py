55e4e67b5166c48b4a578a85b84900d010fbf78b
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-08-18 14:34:01 +0200

    Removed quick tests files from git.

--- quick_tests/clifford_simulation_speed_test.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'Tomasz Rybotycki'
-
-
-strategy = CliffordsRSimulationStrategy(experiment_configuration.initial_number_of_particles,
-                                        experiment_configuration.interferometer_matrix)
-
