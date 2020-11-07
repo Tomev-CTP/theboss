e953a9af49ff00318fa36eb8c236e35f892398f2
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-11-07 01:15:06 +0100

    Added tests for lossy generalized cliffords strategy.

--- src/simulation_strategies/LossyNetworksGeneralizedCliffordsSimulationStrategy.py
+++ src/simulation_strategies/LossyNetworksGeneralizedCliffordsSimulationStrategy.py
@@ -15,11 +15,12 @@ class LossyNetworksGeneralizedCliffordsSimulationStrategy(SimulationStrategy):
             prepare_interferometer_matrix_in_expanded_space(interferometer_matrix))
 
     def simulate(self, input_state: ndarray) -> List[int]:
-        input_state = input_state.reshape(len(input_state), 1)  # Divide by two, coz we have 2N x 2N matrix
+        input_state = input_state.reshape(len(input_state), )  # Divide by two, coz we have 2N x 2N matrix
         expansion_zeros = zeros_like(input_state)
         expanded_state = vstack([input_state, expansion_zeros])
+        expanded_state = expanded_state.reshape(2 * len(input_state), )
         output_state = self._helper_strategy.simulate(expanded_state)
         # Trim output state
         while len(output_state) > len(input_state):
-            output_state = delete(output_state, output_state.shape[0] - 1)
+            output_state = delete(output_state, len(output_state) - 1)
         return output_state
