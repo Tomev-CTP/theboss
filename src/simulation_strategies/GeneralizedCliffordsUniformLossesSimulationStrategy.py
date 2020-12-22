452fbabf2107ba523fb6d49067a3c80cf36b3ef5
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-12-22 15:07:57 +0100

    feat(GCliffords): Added distribution calculation Generalized Cliffords with uniform losses simulation.

--- src/simulation_strategies/GeneralizedCliffordsUniformLossesSimulationStrategy.py
+++ src/simulation_strategies/GeneralizedCliffordsUniformLossesSimulationStrategy.py
@@ -1,14 +1,60 @@
 __author__ = "Tomasz Rybotycki"
 
+from typing import List
+
 from random import random
-from numpy import ndarray, power
+from numpy import ndarray, power, array, int64
+from scipy import special
 from src.simulation_strategies.GeneralizedCliffordsSimulationStrategy import GeneralizedCliffordsSimulationStrategy
+from src.LossyBosonSamplingExactDistributionCalculators import BosonSamplingWithUniformLossesExactDistributionCalculator, BosonSamplingExperimentConfiguration
 
 
 class GeneralizedCliffordsUniformLossesSimulationStrategy(GeneralizedCliffordsSimulationStrategy):
     def __init__(self, interferometer_matrix: ndarray, uniform_losses: float = 0):
         self._uniform_losses = uniform_losses
+        self._distribution = []
+        self._possible_outputs = []
+        self._binomial_weights = []
         super().__init__(interferometer_matrix=interferometer_matrix)
+
+    def simulate(self, input_state: ndarray, samples_number: int = 1) -> List[ndarray]:
+        """
+            Returns sample from linear optics experiments given output state.
+
+            :param input_state: Input state in particle basis.
+            :param samples_number: Number of samples to simulate.
+            :return: A resultant state after traversing through interferometer.
+        """
+        self.input_state = input_state
+        self.number_of_input_photons = sum(input_state)
+        self._get_sorted_possible_states()
+        self.pmfs = dict()
+
+        n = sum(input_state)
+        eta = self._uniform_losses
+
+        configuration = BosonSamplingExperimentConfiguration(
+            interferometer_matrix=self.interferometer_matrix,
+            initial_state=input_state,
+            initial_number_of_particles=n,
+            number_of_modes=len(input_state),
+            probability_of_uniform_loss=eta
+        )
+
+        calculator = BosonSamplingWithUniformLossesExactDistributionCalculator(configuration)
+        self._possible_outputs = calculator.get_outcomes_in_proper_order()
+        self._distribution = [0 for _ in self._possible_outputs]
+
+        # Do note that index is actually equal to number of particles left!
+        self._binomial_weights =\
+            [pow(self._uniform_losses, left) * special.binom(n, left) * pow(1 - eta, n - left) for left in n + 1]
+
+        samples = []
+
+        while len(samples) < samples_number:
+            self._fill_r_sample()
+            samples.append(array(self.r_sample, dtype=int64))
+        return samples
 
     def _fill_r_sample(self) -> None:
         """
@@ -24,3 +70,25 @@ class GeneralizedCliffordsUniformLossesSimulationStrategy(GeneralizedCliffordsSi
             if self.current_key not in self.pmfs:
                 self._calculate_new_layer_of_pmfs()
             self._sample_from_latest_pmf()
+
+    def _calculate_new_layer_of_pmfs(self) -> None:
+        number_of_particle_to_sample = sum(self.r_sample) + 1
+        possible_input_states = self._labeled_states[number_of_particle_to_sample]
+        corresponding_k_vectors = [[self.input_state[i] - state[i] for i in range(len(state))]
+                                   for state in possible_input_states]
+        weights = self._calculate_weights_from_k_vectors(array(corresponding_k_vectors, dtype=float))
+        self.possible_outputs[self.current_key] = self._generate_possible_output_states()
+
+        pmf = []
+
+        for output in self.possible_outputs[self.current_key]:
+            pmf.append(0)
+            for i in range(len(possible_input_states)):
+                probability = self._calculate_outputs_probability(possible_input_states[i], output)
+                probability *= weights[i]
+                pmf[-1] += probability
+            for i in range(len(self._possible_outputs)):
+                if all(output == self._possible_outputs[i]) == 0:
+                    self._distribution[i] = pmf[-1] * self._binomial_weights[sum(output)]
+
+        self.pmfs[self.current_key] = pmf
