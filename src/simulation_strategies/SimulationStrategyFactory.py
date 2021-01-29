7b490b0fe42a18982652acfcfa0642473065cded
Tomasz Rybotycki <rybotycki.tomasz+github@gmail.com>
2021-01-29 23:39:18 +0100

    refactor(BoSS): Class renamed.

--- src/simulation_strategies/SimulationStrategyFactory.py
+++ src/simulation_strategies/SimulationStrategyFactory.py
@@ -17,7 +17,7 @@ from src.simulation_strategies.UniformLossSimulationStrategy import UniformLossS
 from src.simulation_strategies.GeneralizedCliffordsUniformLossesSimulationStrategy import GeneralizedCliffordsUniformLossesSimulationStrategy
 
 
-class StrategyTypes(enum.IntEnum):
+class StrategyType(enum.IntEnum):
     FIXED_LOSS = enum.auto()
     UNIFORM_LOSS = enum.auto()
     CLIFFORD_R = enum.auto()
@@ -29,19 +29,19 @@ class StrategyTypes(enum.IntEnum):
 
 class SimulationStrategyFactory:
     def __init__(self, experiment_configuration: BosonSamplingExperimentConfiguration,
-                 strategy_type: StrategyTypes = StrategyTypes.FIXED_LOSS) -> None:
+                 strategy_type: StrategyType = StrategyType.FIXED_LOSS) -> None:
         self._experiment_configuration = experiment_configuration
         self._strategy_type = strategy_type
         self._strategy_mapping = {
-            StrategyTypes.FIXED_LOSS: self._generate_fixed_losses_strategy,
-            StrategyTypes.UNIFORM_LOSS: self._generate_uniform_losses_strategy,
-            StrategyTypes.CLIFFORD_R: self._generate_r_cliffords_strategy,
-            StrategyTypes.GENERALIZED_CLIFFORD: self._generate_generalized_cliffords_strategy,
-            StrategyTypes.LOSSY_NET_GENERALIZED_CLIFFORD: self._generate_lossy_net_generalized_cliffords_strategy,
-            StrategyTypes.GENERALIZED_U_LOSSY_CLIFFORD: self._generate_u_lossy_generalized_cliffords_strategy
+            StrategyType.FIXED_LOSS: self._generate_fixed_losses_strategy,
+            StrategyType.UNIFORM_LOSS: self._generate_uniform_losses_strategy,
+            StrategyType.CLIFFORD_R: self._generate_r_cliffords_strategy,
+            StrategyType.GENERALIZED_CLIFFORD: self._generate_generalized_cliffords_strategy,
+            StrategyType.LOSSY_NET_GENERALIZED_CLIFFORD: self._generate_lossy_net_generalized_cliffords_strategy,
+            StrategyType.GENERALIZED_U_LOSSY_CLIFFORD: self._generate_u_lossy_generalized_cliffords_strategy
         }
 
-    def set_strategy_type(self, strategy_type: StrategyTypes) -> None:
+    def set_strategy_type(self, strategy_type: StrategyType) -> None:
         self._strategy_type = strategy_type
 
     def set_experiment_configuration(self, experiment_configuration: BosonSamplingExperimentConfiguration) -> None:
