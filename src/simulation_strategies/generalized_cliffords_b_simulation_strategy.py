b4cec5356d971a1e1ea1ed5786e6b0b198dba52c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2021-07-19 10:43:21 +0200

    refactor(g_clifford_b): Refactor generalized Cliffords B algorithm strtategy.

--- src/simulation_strategies/generalized_cliffords_b_simulation_strategy.py
+++ src/simulation_strategies/generalized_cliffords_b_simulation_strategy.py
@@ -13,7 +13,9 @@ __author__ = "Tomasz Rybotycki"
 from .generalized_cliffords_simulation_strategy import GeneralizedCliffordsSimulationStrategy, BSPermanentCalculatorInterface
 from numpy import array, ndarray, int64, zeros_like
 from typing import List
-from numpy.random import choice
+from numpy.random import choice, randint
+from ..boson_sampling_utilities.boson_sampling_utilities import modes_state_to_particle_state
+
 
 class GeneralizedCliffordsBSimulationStrategy(GeneralizedCliffordsSimulationStrategy):
 
@@ -33,22 +35,21 @@ class GeneralizedCliffordsBSimulationStrategy(GeneralizedCliffordsSimulationStra
         """
         self.input_state = input_state
         self.number_of_input_photons = sum(input_state)
-        self.pmfs = dict()
+
+        particle_input_state = list(modes_state_to_particle_state(input_state))
 
         samples = []
 
         while len(samples) < samples_number:
             self._current_input = zeros_like(input_state)
-            self._working_input_state = list(input_state)
+            self._working_input_state = particle_input_state.copy()
             self._fill_r_sample()
             samples.append(array(self.r_sample, dtype=int64))
         return samples
 
-    def _calculate_new_layer_of_pmfs(self) -> None:
+    def _compute_pmf(self) -> None:
 
-        self.possible_outputs[self.current_key] = self._generate_possible_output_states()
-
-        pmf = []
+        self.pmf = []
 
         submatrices_permanents = self._get_permanents_of_submatrices()
 
@@ -62,9 +63,11 @@ class GeneralizedCliffordsBSimulationStrategy(GeneralizedCliffordsSimulationStra
                 permanent_added *= self._bs_permanent_calculator.matrix[m][i]
                 permanent += permanent_added
 
-            pmf.append(abs(permanent)**2)
+            self.pmf.append(abs(permanent)**2)
 
-        self.pmfs[self.current_key] = pmf
+        total = sum(self.pmf)
+        self.pmf = [val / total for val in self.pmf]
+
 
     def _get_permanents_of_submatrices(self):
         permanents = []
@@ -87,21 +90,16 @@ class GeneralizedCliffordsBSimulationStrategy(GeneralizedCliffordsSimulationStra
         return permanents
 
     def _fill_r_sample(self) -> None:
-        self.pmfs.clear()
         self.r_sample = [0 for _ in self.input_state]
-        self.current_key = tuple(self.r_sample)
-        self.current_sample_probability = 1
 
         while self.number_of_input_photons > sum(self.r_sample):
             self._update_current_input()
-
-            if self.current_key not in self.pmfs:
-                self._calculate_new_layer_of_pmfs()
-
-            self._sample_from_latest_pmf()
+            self._compute_pmf()
+            self._sample_from_pmf()
 
     def _update_current_input(self):
-        next_particle_mode = choice(range(len(self._working_input_state)),
-                                    p=self._working_input_state / sum(self._working_input_state))
-        self._working_input_state[next_particle_mode] -= 1
-        self._current_input[next_particle_mode] += 1
+        self._current_input[self._working_input_state.pop(randint(0, len(self._working_input_state)))] += 1
+
+    def _sample_from_pmf(self) -> None:
+        m = choice(range(len(self.input_state)), p=self.pmf)
+        self.r_sample[m] += 1
