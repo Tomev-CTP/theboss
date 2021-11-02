ec2cefad1f591d3b34df706f3860eea304b6c7ee
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2021-11-02 13:55:33 +0100

    fix(losses): Fix the method for computing the number of lossy states, by adding int casting.

--- src/boson_sampling_utilities/boson_sampling_utilities.py
+++ src/boson_sampling_utilities/boson_sampling_utilities.py
@@ -161,7 +161,7 @@ def calculate_number_of_possible_lossy_n_particle_m_mode_output_states(n: int,
     """
     states_number = 0
     for N in range(n + 1):
-        states_number += binom(N + m - 1, N)
+        states_number += round(binom(N + m - 1, N))
     return states_number
 
 
