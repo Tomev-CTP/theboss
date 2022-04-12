7861669ab23a265a576a9bc5a9472003fb372347
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2022-04-12 16:17:39 +0200

    update(package): Prepare 2.0.2 version of theboss package, now using setuptools.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='theboss',
+    version='2.0.2',
+    author="Tomasz Rybotycki",
+    author_email="rybotycki.tomasz+theboss@gmail.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description="A package for (Bo)son (S)ampling (S)imulations!",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.7"',
+        'numpy>=1.19.5; python_version >= "3.7"',
+        'scipy>=1.5.4; python_version >= "3.7"',
+        'guancodes>=0.03; python_version >= "3.7"',
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+    ],
+    license="Apache License 2.0.",
+)
--- theboss/__init__.py
+++
@@ -1,3 +0,0 @@
-"""theboss package provides functionalities for classical simulation of BosonSampling."""
-
-__version__ = "1.1.1"
