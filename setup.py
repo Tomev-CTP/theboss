4db0504a672032d079eb4e13c41130a97ab78d79
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2022-04-13 12:38:49 +0200

    fix(package): Prepare 2.0.3 version with fixed guancodes version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='theboss',
-    version='2.0.2',
+    version='2.0.3',
     author="Tomasz Rybotycki",
     author_email="rybotycki.tomasz+theboss@gmail.com",
     long_description=long_description,
