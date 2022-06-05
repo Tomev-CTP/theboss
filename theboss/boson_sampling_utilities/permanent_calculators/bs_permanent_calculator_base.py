2c4efb1fabeaa97694c97c22853d13de391dc258
Tomev <rybotycki.tomasz+github@gmail.com>
2022-06-05 14:05:31 +0200

    refactor(permanents): Add a base class for Guan code-based permanents calculator. Update Ryser's and Chin-Huh's permanent calculator to abide to the new base class.

--- theboss/boson_sampling_utilities/permanent_calculators/bs_permanent_calculator_base.py
+++ theboss/boson_sampling_utilities/permanent_calculators/bs_permanent_calculator_base.py
@@ -6,9 +6,12 @@ __author__ = "Tomasz Rybotycki"
     It takes care of a lot of boilerplate code.
 """
 
-from typing import Optional
+from typing import Optional, List
 
-from numpy import ndarray, int64, array, asarray
+import operator
+from functools import reduce
+
+from numpy import ndarray, int64, array, asarray, zeros, ones, complex128, nonzero
 
 from ..permanent_calculators.bs_permanent_calculator_interface import (
     BSPermanentCalculatorInterface,
@@ -57,13 +60,158 @@ class BSPermanentCalculatorBase(BSPermanentCalculatorInterface, abc.ABC):
 
     def _can_calculation_be_performed(self) -> bool:
         """
-            Checks if calculation can be performed. For this to happen sizes of given
-            matrix and states have to match.
+        Checks if calculation can be performed. For this to happen sizes of given
+        matrix and states have to match.
 
-            :return: Information if the calculation can be performed.
+        :return: Information if the calculation can be performed.
         """
         return (
             self._matrix.shape[0] == self._matrix.shape[1]
             and len(self._output_state) == len(self._input_state)
             and len(self._output_state) <= self._matrix.shape[0]
         )
+
+
+class BSGuanCodeBasedPermanentCalculatorBase(BSPermanentCalculatorBase, abc.ABC):
+    """
+    This is a base class for those permanent calculators that use Guan codes iterations.
+    It contains boilerplate code related to Guan codes iterations.
+    """
+
+    def __init__(
+        self,
+        matrix: ndarray,
+        input_state: Optional[ndarray] = None,
+        output_state: Optional[ndarray] = None,
+    ) -> None:
+        super().__init__(matrix, input_state, output_state)
+
+        # Guan codes-related variables
+        self._r_vector: ndarray = zeros(len(self._input_state), dtype=int)  # g
+        self._code_update_information: ndarray = ones(
+            len(self._input_state), dtype=int
+        )  # u
+        self._position_limits: List[int] = list(self._input_state)  # n
+        self._index_to_update: int = 0
+        self._last_value_at_index: int = 0
+
+        self._binomials_product: int = 1
+        self.permanent: complex128
+
+    def _initialize_permanent_computation(self) -> None:
+        """Prepares the calculator for permanent computation."""
+
+        self._multiplier = 1
+        self._considered_columns_indices = nonzero(self._output_state)[0]
+        self.permanent = complex128(0)
+
+        self._sums = dict()
+
+        self._binomials_product = 1
+        self._initialize_guan_codes_variables()
+
+    def _initialize_guan_codes_variables(self) -> None:
+        """
+        Initializes Guan codes-related variables before the permanents computation.
+        """
+        self._r_vector = zeros(len(self._input_state), dtype=int)  # g
+        self._code_update_information = ones(len(self._input_state), dtype=int)  # u
+        self._position_limits = list(self._input_state)  # n
+
+    def _update_guan_code(self) -> None:
+        """
+        Prepare the class for processing the next Guan code.
+        """
+        self._index_to_update = 0  # i
+        updated_value_at_index = (
+            self._r_vector[0] + self._code_update_information[0]
+        )  # k
+        while (
+            updated_value_at_index > self._position_limits[self._index_to_update]
+            or updated_value_at_index < 0
+        ):
+            self._code_update_information[
+                self._index_to_update
+            ] = -self._code_update_information[self._index_to_update]
+            self._index_to_update += 1
+
+            if self._index_to_update == len(self._r_vector):
+                return
+
+            updated_value_at_index = (
+                self._r_vector[self._index_to_update]
+                + self._code_update_information[self._index_to_update]
+            )
+
+        self._last_value_at_index = self._r_vector[self._index_to_update]
+        self._r_vector[self._index_to_update] = updated_value_at_index
+
+    def _update_binomials_product(self) -> None:
+        """
+        Update the binomials product to reflect the new Guan code instead of recomputing
+        it.
+        """
+        if self._r_vector[self._index_to_update] > self._last_value_at_index:
+            self._binomials_product *= (
+                self._input_state[self._index_to_update] - self._last_value_at_index
+            ) / self._r_vector[self._index_to_update]
+        else:
+            self._binomials_product *= self._last_value_at_index / (
+                self._input_state[self._index_to_update]
+                - self._r_vector[self._index_to_update]
+            )
+
+    def compute_permanent(self) -> complex128:
+        """
+        This is the main method of the calculator. Assuming that input state,
+        output state and the matrix are defined correctly (that is we've got m x m
+        matrix, and vectors of with length m) this calculates the permanent of an
+        effective scattering matrix related to probability of obtaining output state
+        from a given input state.
+
+        Most of the Guan code-related have essentially the same structure, up to some
+        minor differences.
+
+        :return: Permanent of effective scattering matrix.
+        """
+        if not self._can_calculation_be_performed():
+            raise AttributeError
+
+        self._initialize_permanent_computation()
+
+        # Rest of the steps.
+        while self._r_vector[-1] <= self._position_limits[-1]:
+
+            self._update_guan_code()
+
+            if self._index_to_update == len(self._r_vector):
+                return self._return_permanent()
+
+            # START PERMANENT UPDATE
+            self._multiplier = -self._multiplier
+            self._update_sums()
+            self._update_binomials_product()
+            self._update_permanent()
+
+        return self._return_permanent()
+
+    def _update_permanent(self) -> None:
+        """Update permanent with new addend from r_vector."""
+        self.permanent += reduce(
+            operator.mul,
+            [
+                pow(self._sums[j], self._output_state[j])
+                for j in self._considered_columns_indices
+            ],
+            self._multiplier * self._binomials_product,
+        )
+
+    @abc.abstractmethod
+    def _update_sums(self) -> None:
+        """An abstract method for sums update."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _return_permanent(self) -> complex128:
+        """Some methods require additional operations"""
+        raise NotImplementedError
--- theboss/boson_sampling_utilities/permanent_calculators/ryser_permanent_calculator.py
+++ theboss/boson_sampling_utilities/permanent_calculators/ryser_permanent_calculator.py
@@ -2,28 +2,32 @@ __author__ = "Tomasz Rybotycki"
 
 """
     This file holds the implementation of Ryser method of permanent calculation, as
-    presented in [Cliffords2020] with using Guan codes to speed it up. I modified it
-    slightly, so that the permanent can be used for computing permanents for bunched and
+    presented in [5] with using Guan codes to speed it up. I modified it slightly, so
+    that the permanent can be used for computing permanents for bunched and 
     not-continuous (like [1, 1, 0, 1, 0]) inputs.
 """
 
-import operator
-from functools import reduce
-from typing import Optional
+from typing import Optional, Dict
 
-from numpy import complex128, ndarray, nonzero, zeros, ones
+from numpy import complex128, ndarray
 
 from ..permanent_calculators.bs_permanent_calculator_base import (
-    BSPermanentCalculatorBase,
+    BSGuanCodeBasedPermanentCalculatorBase,
 )
 
 
-class RyserPermanentCalculator(BSPermanentCalculatorBase):
+class RyserPermanentCalculator(BSGuanCodeBasedPermanentCalculatorBase):
     """
     This class is designed to calculate permanent of effective scattering matrix of
     a boson sampling instance. Note, that it can be used to calculate permanent of
     a given matrix. All that is required that input and output states are correct
     representations of Fock states with proper dimensions.
+
+    An important thing to note is that in order to use general approach provided by
+    the boilerplate code in BSGuanCodeBasedPermanentCalculatorBase, we've got to
+    change the role of input and output states. This can be done if we transpose the
+    matrix. Look into BSCCRyserSubmatricesPermanentCalculator class description
+    for more details.
     """
 
     def __init__(
@@ -33,108 +37,43 @@ class RyserPermanentCalculator(BSPermanentCalculatorBase):
         output_state: Optional[ndarray] = None,
     ) -> None:
         super().__init__(matrix, input_state, output_state)
+        self._multiplier: int
+        self._considered_columns_indices: ndarray
+        self.permanent: complex128
+        self._sums: Dict[int, complex128]
+        self._matrix_t: ndarray = matrix.transpose()
 
-    def compute_permanent(self) -> complex128:
+    @property
+    def matrix(self) -> ndarray:
+        return self._matrix
+
+    @matrix.setter
+    def matrix(self, matrix: ndarray) -> None:
+        self._matrix = matrix
+        self._matrix_t = matrix.transpose()
+
+    def _initialize_permanent_computation(self) -> None:
+        """Prepares the calculator for permanent computation."""
+        super()._initialize_permanent_computation()
+        self._multiplier = pow(-1, sum(self._input_state))
+
+        for j in self._considered_columns_indices:
+            self._sums[j] = 0
+
+        self._update_permanent()
+
+    def _update_sums(self) -> None:
         """
-        This is the main method of the calculator. Assuming that input state, output
-        state and the matrix are defined correctly (that is we've got m x m matrix,
-        and vectors of with length m) this calculates the permanent of an effective
-        scattering matrix related to probability of obtaining output state from
-        a given input state.
-
-        :return: Permanent of effective scattering matrix.
+        Update sums instead of recomputing them.
         """
-        if not self._can_calculation_be_performed():
-            raise AttributeError
+        for j in self._sums:
+            self._sums[j] += (
+                self._r_vector[self._index_to_update] - self._last_value_at_index
+            ) * self._matrix_t[self._index_to_update][j]
 
-        r_vector = zeros(len(self._output_state), dtype=int)  # g
-        code_update_information = ones(len(self._output_state), dtype=int)  # u
-        position_limits = list(self._output_state)  # n
-
-        permanent = complex128(0)
-        sums = dict()
-
-        considered_columns_indices = nonzero(self.input_state)[0]
-
-        multiplier = 1
-        binomials_product = 1
-
-        # Initialization (0-th step).
-        for i in considered_columns_indices:
-            sums[i] = 0
-            for j in range(len(self._input_state)):
-                sums[i] += self._input_state[j] * self._matrix[i][j]
-
-        permanent += (
-            multiplier
-            * binomials_product
-            * reduce(
-                operator.mul,
-                [
-                    pow(sums[i], self._input_state[i])
-                    for i in considered_columns_indices
-                ],
-                1,
-            )
-        )
-
-        while r_vector[-1] <= position_limits[-1]:
-
-            # UPDATE R VECTOR
-            index_to_update = 0  # i
-            updated_value_at_index = r_vector[0] + code_update_information[0]  # k
-            while (
-                updated_value_at_index > position_limits[index_to_update]
-                or updated_value_at_index < 0
-            ):
-                code_update_information[index_to_update] = -code_update_information[
-                    index_to_update
-                ]
-                index_to_update += 1
-
-                if index_to_update == len(r_vector):
-                    permanent *= pow(-1, sum(self._input_state))
-                    return permanent
-
-                updated_value_at_index = (
-                    r_vector[index_to_update] + code_update_information[index_to_update]
-                )
-
-            last_value_at_index = r_vector[index_to_update]
-            r_vector[index_to_update] = updated_value_at_index
-            # END UPDATE
-
-            # START PERMANENT UPDATE
-            multiplier = -multiplier
-
-            # Sums update
-            for j in sums:
-                sums[j] += (
-                    r_vector[index_to_update] - last_value_at_index
-                ) * self.matrix[index_to_update][j]
-
-            # Binoms update
-            if r_vector[index_to_update] > last_value_at_index:
-                binomials_product *= (
-                    self._output_state[index_to_update] - last_value_at_index
-                ) / r_vector[index_to_update]
-            else:
-                binomials_product *= last_value_at_index / (
-                    self._output_state[index_to_update] - r_vector[index_to_update]
-                )
-
-            permanent += (
-                multiplier
-                * binomials_product
-                * reduce(
-                    operator.mul,
-                    [
-                        pow(sums[j], self._input_state[j])
-                        for j in considered_columns_indices
-                    ],
-                    1,
-                )
-            )
-
-        permanent *= pow(-1, sum(self._input_state))
-        return permanent
+    def _return_permanent(self) -> complex128:
+        """
+        Given that the (-1)^n factor is taken care in the self._multiplier
+        initialization no other operations are required when returning the permanent.
+        """
+        return self.permanent
