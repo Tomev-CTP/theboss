663a6719b4c9164c40e4af50bc0cecc386b66a2d
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2022-06-13 15:41:46 +0200

    update(version): Update theboss version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="theboss",
-    version="2.1.0",
+    version="2.1.1",
     author="Tomasz Rybotycki",
     author_email="rybotycki.tomasz+theboss@gmail.com",
     long_description=long_description,
