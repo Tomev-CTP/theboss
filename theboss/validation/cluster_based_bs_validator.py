53525aabecaa317c5e88aed8b1ba3bd4404c1ea8
Tomev <rybotycki.tomasz+github@gmail.com>
2022-08-19 14:47:12 +0200

    feat(validation): Add initial version of cluster based validator.

--- theboss/validation/cluster_based_bs_validator.py
+++ theboss/validation/cluster_based_bs_validator.py
@@ -9,6 +9,7 @@ from typing import Sequence, Tuple, List
 from sklearn.cluster import KMeans
 from math import ceil
 from numpy import array
+from scipy.stats import chisquare
 
 
 class ClusterBasedBSValidator:
@@ -17,12 +18,13 @@ class ClusterBasedBSValidator:
     case only KMeans) as presented in the [7].
     """
 
-    def __init__(self, modes_number: int) -> None:
+    def __init__(self, modes_number: int, rejection_threshold: float = 0.5) -> None:
         self._modes_number: int = modes_number
 
         # We use the middle of the rule of the thumb approach from [7] to select the
         # number of clusters.
         self._clusters_number: int = ceil(3 * modes_number / 4)
+        self._rejection_threshold: float = rejection_threshold
 
     @property
     def modes_number(self) -> int:
@@ -64,8 +66,6 @@ class ClusterBasedBSValidator:
                 trusted_sample_assignment.count(i) + tested_sample_assignment.count(i)
             )
 
-        # Note that e is computed differently than in [7].
-        # TODO: Check with MO if that's correct.
         e: List[List[float]] = []
 
         for i in range(self._clusters_number):
@@ -73,18 +73,129 @@ class ClusterBasedBSValidator:
             for j in range(len(samples_sizes)):
                 e[i].append(
                     samples_sizes[j]
-                    * trusted_sample_assignment.count(i)
-                    / len(trusted_sample)
+                    * clusters_total_occupations[i]
+                    / self._clusters_number
                 )
 
-        chi_squared: float = 0
+        # Flatten the lists for the chi squared test
+        observed: List[int] = []
+        expected: List[float] = []
 
         for i in range(self._clusters_number):
             for j in range(2):
-                chi_squared += (
-                    pow(samples_cluster_occupations[i][j] - e[i][j], 2) / e[i][j]
+                observed.append(samples_cluster_occupations[i][j])
+                expected.append(e[i][j])
+
+        print(chisquare(observed, expected))
+        return False
+
+    def validate_tr(
+        self,
+        trusted_sample: Sequence[Tuple[int, ...]],
+        tested_sample: Sequence[Tuple[int, ...]],
+    ) -> bool:
+        k_means = KMeans(n_clusters=self._clusters_number, random_state=0)
+        k_means.fit(array(trusted_sample))
+
+        trusted_sample_assignment: List[int] = list(
+            k_means.predict(array(trusted_sample))
+        )
+
+        tested_sample_assignment: List[int] = list(
+            k_means.predict(array(tested_sample))
+        )
+
+        # Additional variable names are from [7].
+        clusters_total_occupations: List[int] = []  # N_i
+        samples_sizes: List[int] = [len(trusted_sample), len(tested_sample)]  # N_j
+
+        samples_cluster_occupations: List[List[int]] = []  # N_ij
+
+        for i in range(self._clusters_number):
+            samples_cluster_occupations.append(list())
+            samples_cluster_occupations[-1].append(trusted_sample_assignment.count(i))
+            samples_cluster_occupations[-1].append(tested_sample_assignment.count(i))
+
+        for i in range(self._clusters_number):
+            clusters_total_occupations.append(
+                trusted_sample_assignment.count(i) + tested_sample_assignment.count(i)
+            )
+
+        e: List[List[float]] = []
+
+        for i in range(self._clusters_number):
+            e.append(list())
+            for j in range(len(samples_sizes)):
+                e[i].append(
+                    samples_sizes[j]
+                    * samples_cluster_occupations[i][0]
+                    / samples_sizes[0]
                 )
 
-        # TODO FROM HERE
-        print(chi_squared)
-        return False
+        # Flatten the lists for the chi squared test
+        observed: List[int] = []
+        expected: List[float] = []
+
+        for i in range(self._clusters_number):
+            for j in range(2):
+                observed.append(samples_cluster_occupations[i][j])
+                expected.append(e[i][j])
+
+        statistic, p_value = chisquare(observed, expected)
+        print((statistic, p_value))
+        return p_value > self._rejection_threshold
+
+    def validate_tr2(
+        self,
+        trusted_sample: Sequence[Tuple[int, ...]],
+        tested_sample: Sequence[Tuple[int, ...]],
+    ) -> bool:
+        k_means = KMeans(n_clusters=self._clusters_number, random_state=0)
+        k_means.fit(array(trusted_sample))
+
+        trusted_sample_assignment: List[int] = list(
+            k_means.predict(array(trusted_sample))
+        )
+
+        tested_sample_assignment: List[int] = list(
+            k_means.predict(array(tested_sample))
+        )
+
+        # Additional variable names are from [7].
+        clusters_total_occupations: List[int] = []  # N_i
+        samples_sizes: List[int] = [len(trusted_sample), len(tested_sample)]  # N_j
+
+        samples_cluster_occupations: List[List[int]] = []  # N_ij
+
+        for i in range(self._clusters_number):
+            samples_cluster_occupations.append(list())
+            samples_cluster_occupations[-1].append(trusted_sample_assignment.count(i))
+            samples_cluster_occupations[-1].append(tested_sample_assignment.count(i))
+
+        for i in range(self._clusters_number):
+            clusters_total_occupations.append(
+                trusted_sample_assignment.count(i) + tested_sample_assignment.count(i)
+            )
+
+        e: List[List[float]] = []
+
+        for i in range(self._clusters_number):
+            e.append(list())
+            for j in range(len(samples_sizes)):
+                e[i].append(
+                    samples_sizes[j]
+                    * samples_cluster_occupations[i][0]
+                    / samples_sizes[0]
+                )
+
+        # Flatten the lists for the chi squared test
+        observed: List[int] = []
+        expected: List[float] = []
+
+        for i in range(self._clusters_number):
+            observed.append(samples_cluster_occupations[i][1])
+            expected.append(e[i][1])
+
+        statistic, p_value = chisquare(observed, expected)
+        print((statistic, p_value))
+        return p_value > self._rejection_threshold
