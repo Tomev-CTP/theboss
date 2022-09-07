4c743f1abf07af47db05510e11398fb7e62cba73
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2022-09-07 13:46:01 +0200

    fix(validator): Fix the cluster based validator. It now validates the samples with a slight modification to the original formula, which allows it to perform with the accuracy as stated in the paper.

--- theboss/validation/cluster_based_bs_validator.py
+++ theboss/validation/cluster_based_bs_validator.py
@@ -74,62 +74,8 @@ class ClusterBasedBSValidator:
                 e[i].append(
                     samples_sizes[j]
                     * clusters_total_occupations[i]
-                    / self._clusters_number
-                )
-
-        # Flatten the lists for the chi squared test
-        observed: List[int] = []
-        expected: List[float] = []
-
-        for i in range(self._clusters_number):
-            for j in range(2):
-                observed.append(samples_cluster_occupations[i][j])
-                expected.append(e[i][j])
-
-        print(chisquare(observed, expected))
-        return False
-
-    def validate_tr(
-        self,
-        trusted_sample: Sequence[Tuple[int, ...]],
-        tested_sample: Sequence[Tuple[int, ...]],
-    ) -> bool:
-        k_means = KMeans(n_clusters=self._clusters_number, random_state=0)
-        k_means.fit(array(trusted_sample))
-
-        trusted_sample_assignment: List[int] = list(
-            k_means.predict(array(trusted_sample))
-        )
-
-        tested_sample_assignment: List[int] = list(
-            k_means.predict(array(tested_sample))
-        )
-
-        # Additional variable names are from [7].
-        clusters_total_occupations: List[int] = []  # N_i
-        samples_sizes: List[int] = [len(trusted_sample), len(tested_sample)]  # N_j
-
-        samples_cluster_occupations: List[List[int]] = []  # N_ij
-
-        for i in range(self._clusters_number):
-            samples_cluster_occupations.append(list())
-            samples_cluster_occupations[-1].append(trusted_sample_assignment.count(i))
-            samples_cluster_occupations[-1].append(tested_sample_assignment.count(i))
-
-        for i in range(self._clusters_number):
-            clusters_total_occupations.append(
-                trusted_sample_assignment.count(i) + tested_sample_assignment.count(i)
-            )
-
-        e: List[List[float]] = []
-
-        for i in range(self._clusters_number):
-            e.append(list())
-            for j in range(len(samples_sizes)):
-                e[i].append(
-                    samples_sizes[j]
-                    * samples_cluster_occupations[i][0]
-                    / samples_sizes[0]
+                    # / self._clusters_number  # As in [7]
+                    / sum(samples_sizes)  # My hunch.
                 )
 
         # Flatten the lists for the chi squared test
@@ -145,11 +91,39 @@ class ClusterBasedBSValidator:
         print((statistic, p_value))
         return p_value > self._rejection_threshold
 
-    def validate_tr2(
+    def validate_majority_voting(
         self,
         trusted_sample: Sequence[Tuple[int, ...]],
         tested_sample: Sequence[Tuple[int, ...]],
     ) -> bool:
+        """
+        TODO if that's the one we will use.
+
+        :param trusted_sample:
+        :param tested_sample:
+        :return:
+        """
+        repetitions: int = 11
+        successes: int = 0
+
+        for _ in range(repetitions):
+            if self.validate(trusted_sample, tested_sample):
+                successes += 1
+
+        return successes > repetitions // 2
+
+    def validate_tr(
+        self,
+        trusted_sample: Sequence[Tuple[int, ...]],
+        tested_sample: Sequence[Tuple[int, ...]],
+    ) -> bool:
+        """
+        TODO if that's the one that we will use in the end.
+
+        :param trusted_sample:
+        :param tested_sample:
+        :return:
+        """
         k_means = KMeans(n_clusters=self._clusters_number, random_state=0)
         k_means.fit(array(trusted_sample))
 
@@ -199,3 +173,24 @@ class ClusterBasedBSValidator:
         statistic, p_value = chisquare(observed, expected)
         print((statistic, p_value))
         return p_value > self._rejection_threshold
+
+    def validate_tr_majority_voting(
+        self,
+        trusted_sample: Sequence[Tuple[int, ...]],
+        tested_sample: Sequence[Tuple[int, ...]],
+    ) -> bool:
+        """
+        TODO if that's the one we will use.
+
+        :param trusted_sample:
+        :param tested_sample:
+        :return:
+        """
+        repetitions: int = 11
+        successes: int = 0
+
+        for _ in range(repetitions):
+            if self.validate_tr(trusted_sample, tested_sample):
+                successes += 1
+
+        return successes > repetitions // 2
